857e33d68a2c2f0fd961b73677d5648f5cb6a5da
Elif <elifnurbirer1994@gmail.com>
2022-12-23 19:29:21 -0800

    result1 commit

--- src/main/java/org/example/day1/Day1.java
+++ src/main/java/org/example/day1/Day1.java
@@ -3,5 +3,8 @@ package org.example.day1;
 public class Day1 {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
     }
 }
